f62923c6e0b925b16408cbc7182e43465b2785aa
santosh <kolisantosh222@gmail.com>
2024-02-17 23:10:57 +0530

    databasehelper updated

--- lib/app/data/dabasehelper.dart
+++ lib/app/data/dabasehelper.dart
@@ -2,8 +2,10 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
-  getDatabase() async {
-    final database = openDatabase(
+  late Database _db;
+
+  Future<void> init() async {
+    _db = await openDatabase(
       join(await getDatabasesPath(), 'todo_database.db'),
       onCreate: (db, version) {
         return db.execute(
@@ -12,6 +14,37 @@ class DatabaseHelper {
       },
       version: 1,
     );
-    return database;
+  }
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    return await _db.insert('todo', row);
+  }
+
+  // All of the rows are returned as a list of maps, where each map is
+  // a key-value list of columns.
+  Future<List<Map<String, dynamic>>> queryAllRows() async {
+    return await _db.query('todo');
+  }
+
+  // We are assuming here that the id column in the map is set. The other
+  // column values will be used to update the row.
+  Future<int> update(Map<String, dynamic> row) async {
+    int id = row['id'];
+    return await _db.update(
+      'todo',
+      row,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  // Deletes the row specified by the id. The number of affected rows is
+  // returned. This should be 1 as long as the row exists.
+  Future<int> delete(int id) async {
+    return await _db.delete(
+      'todo',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
   }
 }
